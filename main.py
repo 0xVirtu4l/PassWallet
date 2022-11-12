618ee5465610f0fdb4753564df40e5954508a1d6
Marwan <76711572+Marwan712-Design@users.noreply.github.com>
2022-11-12 12:01:05 +0200

    Update main.py
    Add New Features For The Program 
    1- Added A Notification to notify you that the generated password has been copied to your clipboard
    2- Added Some Code Errors
    3- Added Search in the database file to get your password and email 
    4-change the database file extension from 'txt' to 'JSON'
    5-Added some exceptions for the code

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generatePassowrd():
+    copy_label = Label(text="Password Copied Successfully to Clipboard", fg="green", font="Helvetica20")
+    copy_label.grid(row=5, column=1, columnspan=2, sticky="EW")
     password_entry.delete(0, END)
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -18,22 +21,56 @@ def generatePassowrd():
     password = "".join(password_list)
     password_entry.insert(0,password)
     pyperclip.copy(password)
+     # *****  Update V2 ****** #
+    copy_label.after(2000,copy_label.destroy)
+     # *****  Update V2 ****** #
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def savePassword():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    if len(website) == 0 or len(email) == 0 or password==0:
-        messagebox.showinfo(title="Oops there is an error",message="Erorr Code: Elx10093\n Please make sure you haven't left any fields empty.")
+    new_data={website:{"email":email,"password":password,}}
+    if len(website) == 0 or len(email) == 0 or len(password)==0:
+        messagebox.showinfo(title="Oops there is an error",message="Erorr Code: PWER_001\n Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website,message=f"These are the details entered: \n Email: {email} \n Password: {password} \n Is it ok to save ?")
-        if is_ok:
-            with open("passSave.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0,END)
-                email_entry.delete(0,END)
-                password_entry.delete(0,END)
-
+    # *****  Update V2 ****** #
+        try:
+          with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+         with open("data.json","w") as data_file:
+             json.dump(new_data,data_file,indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+             json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+    # ***** Update V2 ****** #
+# ---------------------------- FIND PASSWORD ------------------------------- #
+# *****  Update V2 ****** #
+def find_password():
+    website = website_entry.get()
+    try :
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops there is an error",message="Erorr Code: PWER_002\n No Data File FOUND !!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            email_entry.delete(0, END)
+            email_entry.insert(0, email)
+            password_entry.delete(0, END)
+            password_entry.insert(0, password)
+        if len(website) ==0:
+            messagebox.showinfo(title="Oops there is an error",message="Erorr Code: PWER_001\n Please make sure you haven't left any fields empty.")
+        elif website not in data:
+            messagebox.showinfo(title="Oops there is an error", message="Erorr Code: PWER_003\n The Email and The Password Are Not In The Database!!")
+# *****  Update V2 ****** #
 
 # ---------------------------- UI SETUP ------------------------------- #
 window  = Tk()
@@ -52,9 +89,11 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
+
+
 #Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2,sticky="EW",pady=4)
+website_entry.grid(row=1, column=1,sticky="EW",pady=4)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2,sticky="EW",pady=4)
@@ -63,6 +102,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1,sticky="EW",pady=4)
 
 # Buttons
+search_button = Button(text="Search",command=find_password)
+search_button.grid(row=1, column=2,sticky="EW",pady=4,padx=4)
 generate_password_button = Button(text="Generate Password",command=generatePassowrd)
 generate_password_button.grid(row=3, column=2,sticky="EW",pady=4,padx=2)
 add_button = Button(text="Add", width=36,command=savePassword)
